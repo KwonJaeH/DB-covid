88c7c1b473e3ea661dad12ce58108f88921e84d1
KwonJaeH <81853056+KwonJaeH@users.noreply.github.com>
2021-12-01 23:47:43 +0900

    Update Parsing_Hospital to Patient.py

--- Make Webpage/Make Hospitals for each patient, Google Map webpage/Parsing_Hospital to Patient.py
+++ Make Webpage/Make Hospitals for each patient, Google Map webpage/Parsing_Hospital to Patient.py
@@ -17,11 +17,26 @@ mydb = mysqldb.connect(host='127.0.0.1',
                        db=dbname)
 cursor = mydb.cursor()
 
-sql = "select patient_id,province,city from PATIENT_INFO"
 
+# 해댱 province,city 에서 가장 가까운 병원 위치
+position = {}
+# 각 병원(Hospital_id) 당 capacity , current
+h_capacity= {}
+
+# capacity 설정
+sql = "select hospital_id,capacity,current from HOSPITAL"
 cursor.execute(sql)
 
-position = {}
+for row in cursor :
+    id = row[0]
+    capacity = row[1]
+    current = row[2]
+
+    h_capacity[id] = (capacity,current)
+
+
+sql = "select patient_id,province,city from PATIENT_INFO"
+cursor.execute(sql)
 
 for row in cursor:
 
@@ -47,11 +62,20 @@ for row in cursor:
 
     print(province, city)
 
-    # 이미 province, city 해당하는 병원을 구했으면 continue
+    # 이미 province, city 해당하는 병원을 구했고 남은 자리가 있다면 continue
     if (province, city) in position:
-        sql = "UPDATE PATIENT_INFO SET Hospital_id = %d where patient_id = %d" % (position[(province, city)], id)
-        cur.execute(sql)
-        continue
+        h_id = position[(province,city)]
+        if h_capacity[h_id][0] > 0 :
+            sql = "UPDATE PATIENT_INFO SET Hospital_id = %d where patient_id = %d" % (h_id, id)
+            cur.execute(sql)
+
+            capacity = h_capacity[h_id][0] - 1
+            current = h_capacity[h_id][1] + 1
+            h_capacity[h_id] = [capacity, current]
+            
+            sql = "UPDATE HOSPITAL SET capacity = %d, current = %d where hospital_id = %d" %(capacity,current,h_id)
+            cur.execute(sql)
+            continue
 
     sql = "select latitude, longitude from REGION where province = '%s' and city = '%s'" % (province, city)
     cur.execute(sql)
@@ -75,11 +99,19 @@ for row in cursor:
         dist = math.sqrt((a * a) + (b * b))
 
         if dist < now_dist:
-            now_dist = dist
-            near_Hid = h_id
-
+            if h_capacity[h_id][0] > 0 :
+                now_dist = dist
+                near_Hid = h_id
+    
     position[(province, city)] = near_Hid
     sql = "UPDATE PATIENT_INFO SET Hospital_id = %d where patient_id = %d" % (near_Hid, id)
+    cur.execute(sql)
+
+    capacity = h_capacity[near_Hid][0] - 1
+    current = h_capacity[near_Hid][1] + 1
+    h_capacity[near_Hid] = [capacity, current]
+            
+    sql = "UPDATE HOSPITAL SET capacity = %d, current = %d where hospital_id = %d" % (capacity,current,near_Hid)
     cur.execute(sql)
 
     # print(cursor)
