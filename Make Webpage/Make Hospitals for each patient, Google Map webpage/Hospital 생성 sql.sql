eaeda540530004b4359fe641d64ee2e1f526d2d2
KwonJaeH <81853056+KwonJaeH@users.noreply.github.com>
2021-12-01 21:56:26 +0900

    Update Hospital 생성 sql.sql

--- Make Webpage/Make Hospitals for each patient, Google Map webpage/Hospital 생성 sql.sql
+++ Make Webpage/Make Hospitals for each patient, Google Map webpage/Hospital 생성 sql.sql
@@ -10,20 +10,3 @@ CREATE TABLE HOSPITAL(
 	PRIMARY KEY (Hospital_id)
 	);
 
-
-CREATE TABLE ALL_REGION(
-	Region_code INT NOT NULL,
-	Province VARCHAR(50),
-	City VARCHAR(50),
-	Latitude FLOAT,
-	Longitude FLOAT,
-	Elementary_school_count INT,
-	Kindergarten_count INT,
-	University_count INT,
-	Academy_ratio FLOAT,
-	Elderly_population_ratio FLOAT,
-	Elderly_alone_ratio FLOAT,
-	Nursing_home_count INT,
-	PRIMARY KEY (Region_code)
-	);
-
